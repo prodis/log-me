c11b35aa3a9b4c854e3dbbf72033fc9c7786668b
Prodis <prodis@gmail.com>
2011-08-24 01:02:26 -0300

    LogMe module.

---
+++ lib/log-me.rb
@@ -0,0 +1,35 @@
+# encoding: UTF-8
+require 'rubygems'
+require 'logger'
+
+module LogMe
+  attr_writer :log_enabled
+  attr_writer :log_level
+  attr_writer :logger
+
+  def log_enabled?
+    @log_enabled != false
+  end
+
+  def log_level
+    @log_level ||= :info
+  end
+
+  def logger
+    @logger ||= ::Logger.new STDOUT
+  end
+
+  def log(message)
+    logger.send log_level, message if log_enabled?
+  end
+
+  def self.extended(base)
+    base.send :extend, Configuration
+  end
+
+  module Configuration
+    def configure_me
+      yield self if block_given?
+    end
+  end
+end
--- spec/log-me_spec.rb
+++ spec/log-me_spec.rb
@@ -1,7 +1,103 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+# encoding: UTF-8
+require 'spec_helper'
 
-describe "LogMe" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+module FakeModule
+  extend LogMe
+end
+
+describe LogMe do
+  describe "#log_enabled?" do
+    it "default is true" do
+      FakeModule.log_enabled?.should be_true
+    end
+
+    context "when log is disabled" do
+      around do |example|
+        FakeModule.configure_me { |config| config.log_enabled = false }
+        example.run
+        FakeModule.configure_me { |config| config.log_enabled = true }
+      end
+
+      it "returns false" do
+        FakeModule.log_enabled?.should be_false
+      end
+    end
+  end
+
+  describe "#log_level" do
+    it "default is info" do
+      FakeModule.log_level.should == :info
+    end
+
+    context "when log level is set" do
+      around do |example|
+        FakeModule.configure_me { |config| config.log_level = :debug }
+        example.run
+        FakeModule.configure_me { |config| config.log_level = :info }
+      end
+
+      it "returns set level" do
+        FakeModule.log_level.should == :debug
+      end
+    end
+  end
+
+  describe "#logger" do
+    it "default is Logger" do
+      FakeModule.logger.should be_a(::Logger)
+    end
+
+    context "when set logger" do
+      it "returns set logger" do
+        fake_logger = Class.new
+        FakeModule.configure_me { |config| config.logger = fake_logger }
+        FakeModule.logger.should == fake_logger
+      end
+    end
+  end
+
+  describe "#log" do
+    before :each do
+      @log_stream = StringIO.new
+      FakeModule.configure_me { |config| config.logger = ::Logger.new(@log_stream) }
+    end
+
+    context "when log is enabled" do
+      it "logs the message" do
+        FakeModule.log("Some message to log.")
+        @log_stream.string.should include("Some message to log.")
+      end
+
+      it "calls log level method" do
+        FakeModule.logger.should_receive(:info).with("Some message to log.")
+        FakeModule.log("Some message to log.")
+      end
+    end
+
+    context "when log is disabled" do
+      around do |example|
+        FakeModule.configure_me { |config| config.log_enabled = false }
+        example.run
+        FakeModule.configure_me { |config| config.log_enabled = true }
+      end
+
+      it "does not log the message" do
+        FakeModule.log("Some message to log.")
+        @log_stream.string.should be_empty
+      end
+    end
+
+    context "when log level is set" do
+      around do |example|
+        FakeModule.configure_me { |config| config.log_level = :debug }
+        example.run
+        FakeModule.configure_me { |config| config.log_level = :info }
+      end
+
+      it "calls log level set method" do
+        FakeModule.logger.should_receive(:debug).with("Some message to log.")
+        FakeModule.log("Some message to log.")
+      end
+    end
   end
 end
