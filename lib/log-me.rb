a723939ba6c66b25ccbb36c4562a7bdbc07c571e
Prodis <prodis@gmail.com>
2014-02-18 23:05:03 -0300

    Add label to log messages.

--- lib/log-me.rb
+++ lib/log-me.rb
@@ -4,6 +4,7 @@ require 'logme/configuration'
 module LogMe
   attr_writer :log_enabled
   attr_writer :log_level
+  attr_writer :log_label
   attr_writer :logger
 
   def log_enabled?
@@ -14,12 +15,16 @@ module LogMe
     @log_level ||= :info
   end
 
+  def log_label
+    @log_label ||= self.name
+  end
+
   def logger
     @logger ||= ::Logger.new STDOUT
   end
 
   def log(message)
-    logger.send log_level, "#{message}\n" if log_enabled?
+    logger.send log_level, "[#{log_label}] #{message}\n" if log_enabled?
   end
 
   def self.extended(base)
--- spec/log-me_spec.rb
+++ spec/log-me_spec.rb
@@ -43,6 +43,26 @@ describe LogMe do
     end
   end
 
+  describe "#log_label" do
+    it "default is current module name" do
+      #require 'pry'; binding.pry
+      expect(subject.log_label).to eq subject.name
+    end
+
+    context "when set label" do
+      around do |example|
+        subject.configure { |config| config.log_level = :debug }
+        example.run
+        subject.configure { |config| config.log_level = :info }
+      end
+
+      it "returns label" do
+        subject.configure { |config| config.log_label = "Prodis #15" }
+        expect(subject.log_label).to eq "Prodis #15"
+      end
+    end
+  end
+
   describe "#logger" do
     it "default is Logger" do
       expect(subject.logger).to be_a(::Logger)
@@ -58,20 +78,24 @@ describe LogMe do
   end
 
   describe "#log" do
-    let!(:log_stream) { StringIO.new }
+    let(:log_stream) { StringIO.new }
+    let(:label)      { "Cool Label" }
 
     before do
-      subject.configure { |config| config.logger = ::Logger.new(log_stream) }
+      subject.configure do |config|
+        config.logger = ::Logger.new(log_stream)
+        config.log_label = label
+      end
     end
 
     context "when log is enabled" do
       it "logs the message" do
         subject.log "Some message to log."
-        expect(log_stream.string).to include "Some message to log.\n"
+        expect(log_stream.string).to include "[#{label}] Some message to log.\n"
       end
 
       it "calls log level method" do
-        expect(subject.logger).to receive(:info).with("Some message to log.\n")
+        expect(subject.logger).to receive(:info).with("[#{label}] Some message to log.\n")
         subject.log "Some message to log."
       end
     end
@@ -97,7 +121,7 @@ describe LogMe do
       end
 
       it "calls log level set method" do
-        expect(subject.logger).to receive(:debug).with("Some message to log.\n")
+        expect(subject.logger).to receive(:debug).with("[#{label}] Some message to log.\n")
         subject.log "Some message to log."
       end
     end
